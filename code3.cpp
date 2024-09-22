086acc7858edb335158ecf6613f59e2b10070b04
ManishMandrai <manishkumarmandrai@gmail.com>
2024-09-23 01:33:00 +0530

    .

---
+++ code3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+   int a;
+
+   cout<<"Enter the value of a"<<endl;
+   cin>>a;
+   
+   if(a>0){
+    cout<<"Number is +ve"<<endl;
+   }else if(a<0){
+    cout<<"Number is -ve"<<endl;
+   }else{
+    cout<<"Number is 0"<<endl;
+   }
+
+
+}
