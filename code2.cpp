6dbff24a5b73a2233b659f238345dc5f22d0b230
ManishMandrai <manishkumarmandrai@gmail.com>
2024-09-23 01:16:06 +0530

    .

--- code2.cpp
+++ code2.cpp
@@ -3,9 +3,16 @@ using namespace std;
 
 int main (){
     
-    int n;
-    cin>>n;
-    cout<<"Hello World"<<endl;
+    int a;
+    cin>>a;
+    // cout<<"Hello World"<<endl;
+
+    // if a is +ve
+    if(a>0){
+        cout<<"A is +ve" <<endl;
+    }else{
+        cout<<"A is -ve" <<endl;
+    }
 
     return 0;
 }
